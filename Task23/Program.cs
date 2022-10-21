bd2f82cbeeaf28333bc61099ca4e697cdb152fc4
Michkasov Roman <mihjob@list.ru>
2022-10-22 00:20:44 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23. Напишите программу, 
+//которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+string GetSet(int number)
+{
+    string result = "";
+    for (int index = 1; index < number; index++)
+    {
+        result += Math.Pow(index,3).ToString() + ", ";
+    }
+    result += Math.Pow(number,3).ToString() + ".";
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine()!);
+string set = GetSet(N);
+Console.WriteLine($"Множество кубов целых чисел от 1 до {N}: {set}");
