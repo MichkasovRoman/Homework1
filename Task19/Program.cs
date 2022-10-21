46abfcc4e3b7472c595cff3b3a4e700cec2a4add
Michkasov Roman <mihjob@list.ru>
2022-10-21 21:33:36 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 19. Напишите программу, 
+//которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+int NumberLen (int number) // функция, выдающая количество цифр в заданном числе
+{
+    int count = 0;
+    while (Math.Abs(number) >= 1)
+    {
+        number = Math.Abs(number/10);
+        count ++;
+    }
+    return count;
+}
+
+
+Console.Clear();
+int N = int.Parse(Console.ReadLine()!);
+int M = NumberLen(N);
+Console.Write(M);
